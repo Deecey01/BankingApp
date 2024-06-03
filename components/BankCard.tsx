89c0d301be479b2be05f086fca8706b7e31f3c78
Divyansh Chandak <divyanshchandak2k20.4236@gmail.com>
2024-06-03 19:05:38 +0530

    finalize the app

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,15 +2,16 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Copy from './Copy'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className="bank-card">
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">
-              {userName}
+              {account.name}
             </h1>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
@@ -27,7 +28,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
               </h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-16">1234</span>
+              ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
             </p>
           </article>
         </div>
@@ -57,7 +58,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
         />
       </Link>
 
-      {/* COPY */}
+      {showBalance && <Copy title={account?.shareableId}/>}
     </div>
   )
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils";
@@ -16,13 +16,36 @@ const {
   APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
 } = process.env;
 
+export const getUserInfo=async({userId}:getUserInfoProps)=>{
+  try{
+    const {database}=await createAdminClient();
+    const user=await database.listDocuments(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      [Query.equal('userId',[userId])]
+    )
+    // console.log(user)
+    
+    return parseStringify(user.documents[0]);
+  }catch(error){
+    console.log(error);
+  }
+}
+
 export const signIn = async ({ email, password }: signInProps) => {
   try {
     const { account } = await createAdminClient();
+    const session = await account.createEmailPasswordSession(email, password);
 
-    const response = await account.createEmailPasswordSession(email, password);
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+    const user = await getUserInfo({userId:session.userId});
 
-    return parseStringify(response);
+    return parseStringify(user);
   } catch (error) {
     console.error('Error', error);
   }
@@ -65,7 +88,6 @@ export const signUp = async ({ password, ...userData }: SignUpParams) => {
         dwollaCustomerUrl
       }
     )
-
     const session = await account.createEmailPasswordSession(email, password);
 
     cookies().set("appwrite-session", session.secret, {
@@ -85,7 +107,8 @@ export async function getLoggedInUser() {
   try {
     const { account } = await createSessionClient();
 
-    const user = await account.get();
+    const result = await account.get();
+    const user=await getUserInfo({userId:result.$id});
 
     return parseStringify(user);
   } catch (error) {
@@ -132,7 +155,7 @@ export const createBankAccount = async ({
   accountId,
   accessToken,
   fundingSourceUrl,
-  sharableId,
+  shareableId,
 }: createBankAccountProps) => {
   try {
     const { database } = await createAdminClient();
@@ -147,7 +170,7 @@ export const createBankAccount = async ({
         accountId,
         accessToken,
         fundingSourceUrl,
-        sharableId,
+        shareableId,
       }
     )
 
@@ -204,7 +227,7 @@ export const exchangePublicToken = async ({
       accountId: accountData.account_id,
       accessToken,
       fundingSourceUrl,
-      sharableId: encryptId(accountData.account_id),
+      shareableId: encryptId(accountData.account_id),
     });
 
     // Revalidate the path to reflect the changes
@@ -216,5 +239,50 @@ export const exchangePublicToken = async ({
     });
   } catch (error) {
     console.error("An error occurred while creating exchanging token:", error);
+  }
+}
+
+export const getBanks=async({userId}:getBanksProps)=>{
+  try{
+    const {database}=await createAdminClient();
+    const banks=await database.listDocuments(
+      DATABASE_ID!,
+      BANK_COLLECTION_ID!,
+      [Query.equal('userId',[userId])]
+    )
+
+    return parseStringify(banks.documents);
+  }catch(error){
+    console.log(error);
+  }
+}
+
+export const getBank=async({documentId}:getBankProps)=>{
+  try{
+    const {database}=await createAdminClient();
+    const bank=await database.listDocuments(
+      DATABASE_ID!,
+      BANK_COLLECTION_ID!,
+      [Query.equal('$id',[documentId])]
+    )
+
+    return parseStringify(bank.documents[0]);
+  }catch(error){
+    console.log(error);
+  }
+}
+
+export const getBankByAccountId=async({accountId}:getBankByAccountIdProps)=>{
+  try{
+    const {database}=await createAdminClient();
+    const bank=await database.listDocuments(
+      DATABASE_ID!,
+      BANK_COLLECTION_ID!,
+      [Query.equal('accountId',[accountId])]
+    )
+    if(bank.total!==1) return null;
+    return parseStringify(bank.documents[0]);
+  }catch(error){
+    console.log(error);
   }
 }
